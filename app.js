44d4c09c11d3b7cc6afa4845a6e0bc6da494a54d
Aniruddha Dewangan <ani.dewangan2002@gmail.com>
2023-08-27 18:54:51 +0530

    main

--- app.js
+++ app.js
@@ -1,134 +1,145 @@
 console.log("M");
 
-const data = [
+const game_data = [
      {
-          id: 1,
-          name: 'Double Cheeseburger',
-          category: 'burger',
-          image:
-               'https://images.unsplash.com/photo-1607013251379-e6eecfffe234?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YnVyZ2Vyc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=1400&q=60',
-          price: '$$$$',
+          name: 'valo',
+          url: "http://youtube.com",
      },
      {
-          id: 2,
-          name: 'Bacon Cheeseburger',
-          category: 'burger',
-          image:
-               'https://images.unsplash.com/photo-1553979459-d2229ba7433b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTh8fGJ1cmdlcnN8ZW58MHx8MHx8&auto=format&fit=crop&w=1400&q=60',
-          price: '$',
+          name: 'valorant',
+          url: "http://youtube.com",
+     },
+
+     {
+          name: 'league',
+          url: "http://youtube.com",
      },
      {
-          id: 3,
-          name: 'Mushroom Burger',
-          category: 'burger',
-          image:
-               'https://images.unsplash.com/photo-1608767221051-2b9d18f35a2f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTV8fGJ1cmdlcnN8ZW58MHx8MHx8&auto=format&fit=crop&w=1400&q=60',
-          price: '$$',
+          name: 'league of legends',
+          url: "http://youtube.com",
      },
      {
-          id: 4,
-          name: 'Loaded Burger',
-          category: 'burger',
-          image:
-               'https://images.unsplash.com/photo-1568901346375-23c9450c58cd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8YnVyZ2Vyc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=1400&q=60',
-          price: '$$$',
+          name: 'league legends',
+          url: "http://youtube.com",
+     },
+
+     {
+          name: 'gta',
+          url: "http://youtube.com",
      },
      {
-          id: 5,
-          name: 'Feta & Spinnach',
-          category: 'pizza',
-          image:
-               'https://images.unsplash.com/photo-1593560708920-61dd98c46a4e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8cGl6emF8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60',
-          price: '$$',
+          name: 'gtav',
+          url: "http://youtube.com",
      },
      {
-          id: 6,
-          name: 'Supreme Pizza',
-          category: 'pizza',
-          image:
-               'https://images.unsplash.com/photo-1604382355076-af4b0eb60143?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8cGl6emF8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60',
-          price: '$$$',
+          name: 'gta v',
+          url: "http://youtube.com",
      },
      {
-          id: 7,
-          name: 'Meat Lovers',
-          category: 'pizza',
-          image:
-               'https://images.unsplash.com/photo-1628840042765-356cda07504e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTh8fHBpenphfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60',
-          price: '$$$$',
+          name: 'grand theft auto',
+          url: "http://youtube.com",
      },
      {
-          id: 8,
-          name: 'Cheese Pizza',
-          category: 'pizza',
-          image:
-               'https://images.unsplash.com/photo-1548369937-47519962c11a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8Y2hlZXNlJTIwcGl6emF8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60',
-          price: '$',
+          name: 'grand theft auto v',
+          url: "http://youtube.com",
      },
      {
-          id: 9,
-          name: 'Kale Salad',
-          category: 'salad',
-          image:
-               'https://images.unsplash.com/photo-1515543237350-b3eea1ec8082?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8c2FsYWQlMjBjZWFzYXJ8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60',
-          price: '$$',
+          name: 'grand theft auto 5',
+          url: "http://youtube.com",
      },
      {
-          id: 10,
-          name: 'Ceasar Salad',
-          category: 'salad',
-          image:
-               'https://images.unsplash.com/photo-1546793665-c74683f339c1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8c2FsYWQlMjBjZWFzYXJ8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60',
-          price: '$$$',
+          name: 'gta 5',
+          url: "http://youtube.com",
+     },
+
+     {
+          name: 'csgo',
+          url: "http://youtube.com",
      },
      {
-          id: 11,
-          name: 'Loaded Salad',
-          category: 'salad',
-          image:
-               'https://images.unsplash.com/photo-1540420773420-3366772f4999?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8c2FsYWR8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60',
-          price: '$$$$',
+          name: 'cs:go',
+          url: "http://youtube.com",
      },
      {
-          id: 12,
-          name: 'Fruit Salad',
-          category: 'salad',
-          image:
-               'https://images.unsplash.com/photo-1564093497595-593b96d80180?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8ZnJ1aXQlMjBzYWxhZHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60',
-          price: '$',
+          name: 'cs: go',
+          url: "http://youtube.com",
      },
      {
-          id: 13,
-          name: 'Wings',
-          category: 'chicken',
-          image:
-               'https://images.unsplash.com/photo-1567620832903-9fc6debc209f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8Y2hpY2tlbiUyMGZvb2R8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60',
-          price: '$$',
+          name: 'cs go',
+          url: "http://youtube.com",
      },
      {
-          id: 14,
-          name: 'Baked Chicken',
-          category: 'chicken',
-          image:
-               'https://images.unsplash.com/photo-1594221708779-94832f4320d1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8Y2hpY2tlbiUyMGZvb2R8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60',
-          price: '$$$$',
+          name: 'counter-strike: global offensive',
+          url: "http://youtube.com",
      },
      {
-          id: 15,
-          name: 'Chicken Tenders',
-          category: 'chicken',
-          image:
-               'https://images.unsplash.com/photo-1562967914-608f82629710?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fGNoaWNrZW4lMjBmb29kfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60',
-          price: '$',
+          name: 'counter-strike global offensive',
+          url: "http://youtube.com",
      },
      {
-          id: 16,
-          name: 'Chicken Kabob',
-          category: 'chicken',
-          image:
-               'https://images.unsplash.com/photo-1603360946369-dc9bb6258143?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTN8fGNoaWNrZW4lMjBmb29kfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60',
-          price: '$$$',
+          name: 'counter strike: global offensive',
+          url: "http://youtube.com",
      },
-];
+     {
+          name: 'counter strike global offensive',
+          url: "http://youtube.com",
+     },
+     {
+          name: 'counter-strike',
+          url: "http://youtube.com",
+     },
+     {
+          name: 'counter strike',
+          url: "http://youtube.com",
+     },
+
+     {
+          name: 'apex',
+          url: "http://youtube.com",
+     },
+     {
+          name: 'apex legends',
+          url: "http://youtube.com",
+     },
+]
+
+// const game_data = [
+//      {
+//           name: ['cs:go', 'csgo', 'cs go', 'cs: go', 'cs', 'counter strike', 'counter-strike', 'counter-strike: global offensive', 'counter-strike global offensive', 'counter strike global offensive', 'counter strike: global offensive', 'counter strike: global offensive'],
+//           url: "http://youtube.com",
+//      },
+// ]
+
+function getVal() {
+     const val = document.getElementById("search-input").value;
+     console.log(val);
+     game_data.forEach(redirect);
+
+}
+
+function redirect(item) {
+     if (item = game_data.name) {
+          window.location.href = 'game_data.url';
+     }
+     else {
+          alert("NOT FOUND");
+     }
+}
+
+function getVal() {
+     const val = document.getElementById("search-input").value;
+     console.log(val);
+     redirect(val);
+}
+
+function redirect(inputName) {
+     const foundGame = game_data.find(item => item.name.toLowerCase() === inputName);
+
+     if (foundGame) {
+          window.location.href = foundGame.url;
+     } else {
+          alert("NOT FOUND");
+     }
+}
 
 // D:\GitHub_Repositories\Web_Dev\Streak_GamingWebsite\Img
